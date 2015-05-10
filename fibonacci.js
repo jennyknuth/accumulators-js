198a02ac88bdc9a9b6b4ce07c4474f35673a9bcf
Jenny Knuth <inkboco@gmail.com>
2015-05-09 20:54:20 -0600

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -15,7 +15,7 @@ var fibonacci = function(num){
   }
 };
 
-//var number = parseInt(prompt("Choose a positive number", "0"));
-//for some reason, prompt is not working in node, so I have
-//hardcoded the number. This code works in Snippets.
-fibonacci(888);
+var number = parseInt(prompt("Choose a positive number", "0"));
+
+
+fibonacci(number);
