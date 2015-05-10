ccb96c1906e9b9294fb5f353e222982cb3219f4b
Jenny Knuth <inkboco@gmail.com>
2015-05-09 22:41:08 -0600

    another thought added, still just a sketch

--- sequence.js
+++ sequence.js
@@ -2,9 +2,11 @@
 
 //some ideas (will try on this on May 10)
 
+//either decide they are sorted or determine if numbers are in order or not
+//decide they are sorted
 //find the factors of the biggest number (absolute value)
 //compare to factors of the other numbers
-//compare to index number to see how fast it is growing/declining
+//compare to index number/length to see how fast it is growing/declining
 //each step is equivalent in some way…
 //some pattern with index and factors…
 
