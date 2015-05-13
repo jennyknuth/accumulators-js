dc5f1108df8f116753460f4f5c1b8b1e43499035
Jenny Knuth <inkboco@gmail.com>
2015-05-12 20:32:24 -0600

    fixed up some ideas and comments

--- sequence.js
+++ sequence.js
@@ -1,27 +1,29 @@
 //build a function that takes an arbitrary sequence and tries to find the next number in the sequence
 
-//some ideas (will try on this on May 10)
-
 //either decide they are sorted or determine if numbers are in order or not
 //decide they are sorted
+//
 //find the factors of the biggest number (absolute value)
+//decided this would take a very long nested loop, must be a better way
+//
 //compare to factors of the other numbers
-//compare to index number/length to see how fast it is growing/declining
+//
 //each step is equivalent in some way…
 //some pattern with index and factors…
 
 //a and b make c, (a)b and c make d, ((a)b)c) and d make e, etc.
 //
+//hmmm, didn't end up using any loops or arr[i]…
 
 var linearArray = [0, 3, 6, 9];
 var multArray = [1, 3, 9, 27];
 var squArray = [1, 3, 9, 81];
 
-//check first to see if it is linear
 
 var nextInSequence = function(arr){
   var nextNumber = 0;
   if (arr[1] - arr[0] === arr[2] - arr[1]){
+    //check first to see if it is linear
     nextNumber = arr[arr.length-1] + (arr[2] - arr[1]);
     console.log("next number in sequence is " + nextNumber);
   }
@@ -32,6 +34,7 @@ var nextInSequence = function(arr){
     console.log("next number in sequence is " + nextNumber);
   }
   else if (arr[3] % arr[1] === 0 && arr[3]/arr[1] === arr[1]*arr[2]) {
+    //if increasing exponentially
     nextNumber = (arr[arr.length-1]*arr[arr.length-1]);
     console.log("next number in sequence is " + nextNumber);
   }
